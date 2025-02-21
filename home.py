5f144f43b517b61c84de06fbbee8f35b36307022
Anup Thatal <53684908+AnupThatal@users.noreply.github.com>
2025-02-21 13:19:10 +0545

    Update home.py

--- home.py
+++ home.py
@@ -3,42 +3,80 @@ import pandas as pd
 import folium
 from folium import plugins
 from streamlit_folium import st_folium
+import requests
 
 
-st.set_page_config(
-    page_title='Homepage',
 
-)
+def data_collection():
+    odata_urls = [
+        'https://survey.kuklpid.gov.np/v1/projects/20/forms/kukl_customer_survey_phase1.svc'
+    ]
+    submission_entity_set = 'Submissions'
+    username = 'anupthatal2@gmail.com'
+    password = 'Super@8848'
+    session = requests.Session()
+    session.auth = (username, password)
+    
+    all_dfs = []  # List to store DataFrames from each URL
+    
+    for odata_url in odata_urls:
+        submission_url = f"{odata_url}/{submission_entity_set}"
+        response = session.get(submission_url)
+        if response.status_code == 200:
+            data = response.json()
+            df = pd.DataFrame(data['value'])
+            pd.set_option('display.max_columns', None)
+            print(df)
+            df['lat'] = df['b02'].apply(lambda x: x['coordinates'][1] if isinstance(x, dict) and 'coordinates' in x else None)
+            df['lon'] = df['b02'].apply(lambda x: x['coordinates'][0] if isinstance(x, dict) and 'coordinates' in x else None)
+            customer = []
+            connection = []
+            submittername = []
+            reviewState = []
+            for i in df['gb12_skip']:
+                customer.append(i['gc01_skp1']['gc20']['c20'])
+                connection.append(i['gc01_skp1']['gc20']['c22'])
+            for i in df['__system']:
+                submittername.append(i['submitterName'])
+                reviewState.append(i['reviewState'])
+            # for i in df['gb10-b10_package']:
+            #     packages.append(i['b10_dni']['b10_dmi'])
+            #     print(packages)
+            df['ReviewState'] = reviewState
+            df['SubmitterName'] = submittername
+            df['gb12_skip-gc01_skp1-gc20-c20'] = customer
+            df['gb12_skip-gc01_skp1-gc20-c22'] = connection
+            df['SubmitterName'] = df['SubmitterName'].str.upper()
+            df = df[['b10_dmi','ward_number','unique_form_id','lat','lon','gb12_skip-gc01_skp1-gc20-c20', 'gb12_skip-gc01_skp1-gc20-c22','SubmitterName', 'ReviewState', 'unit_owners']]
+            all_dfs.append(df)  # Append the processed DataFrame
+    final_df = pd.concat(all_dfs, ignore_index=True)
+    return final_df
 
-df = pd.read_csv('df_final.csv')
-df1=pd.read_csv('HHC_Data.csv')
+df = data_collection()
+
 
 # Sidebar
 with st.sidebar:
     
-    location=df1['Areas'].to_list() 
-    location_area=st.selectbox('Select location',location)
-    areas_list = df['b10_sub_dmi'].dropna().unique().tolist()
+    location=df['ward_number'].unique().tolist()
+    # location_area=st.selectbox('Select location',location)
+    areas_list = df['b10_dmi'].dropna().unique().tolist()
     selected_area = st.selectbox('Select Area', areas_list)
     if selected_area:
-        filtered_df = df[df['b10_sub_dmi'] == selected_area]
-        filtered_location_df=df1[df1['Areas']==location_area]
-        P=filtered_location_df['Packages'].values[0]
-        HHC=filtered_location_df['SDMA wise HHC'].values[0]
-        SDMA=filtered_location_df['sDMA'].values[0]
-        ward=filtered_location_df['ward number'].values[0]
-        mun=filtered_location_df['District'].values[0]
-        packages = filtered_df['gb10-b10_package'].values[0]
-        person=filtered_df['Person'].values[0]
-        phone=filtered_df['Phone'].values[0]
-        sub_dmi_counts = filtered_df['b10_sub_dmi'].value_counts()
-        st.write(f"Municipality:blue[{mun}]")
+        filtered_df = df[df['b10_dmi'] == selected_area]
+        # filtered_df=df1[df1['Areas']==location_area]
+        # P=filtered_df['Packages']
+        SDMA=filtered_df['b10_dmi'].unique().tolist()[0]
+        ward=filtered_df['ward_number'].unique().tolist()[0]
+    
+        # person=filtered_df['Person']
+        # phone=filtered_df['Phone']
+        sub_dmi_counts = filtered_df['b10_dmi'].value_counts()
         st.write(f"ward of that areas :blue[{ward}]")
-        st.write(f'Packages of :blue[{packages}]')
+        # st.write(f'Packages of :blue[{packages}]')
         st.write(f"ward of :blue[{SDMA}]")
-        st.write(f'Person responsible :blue[{person}]')
-        st.write(f'Phone number of that person :blue[{phone}]')
-        st.write(f"Counts of HHC:{HHC}")
+        # st.write(f'Person responsible :blue[{person}]')
+        # st.write(f'Phone number of that person :blue[{phone}]')
         st.write(sub_dmi_counts)
 
 # Main content
@@ -47,14 +85,14 @@ col1, = st.columns(1)  # Note the use of comma to unpack the list
 with col1:
     if selected_area:
         # Filter DataFrame for selected area
-        selected_df = df[df['b10_sub_dmi'] == selected_area]
+        selected_df = df[df['b10_dmi'] == selected_area]
 
         # Drop rows with NaN values in location coordinates
-        selected_df = selected_df.dropna(subset=['b02-Latitude', 'b02-Longitude'])
+        selected_df = selected_df.dropna(subset=['lat', 'lon'])
 
         # Extract latitude and longitude lists
-        lat = selected_df['b02-Latitude'].astype(float).tolist()
-        lon = selected_df['b02-Longitude'].astype(float).tolist()
+        lat = selected_df['lat'].astype(float).tolist()
+        lon = selected_df['lon'].astype(float).tolist()
 
         # Calculate the center of the map
         center_lat = sum(lat) / len(lat) if len(lat) > 0 else 0
